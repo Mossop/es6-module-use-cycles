72d18c1cc68221f37893d0695c6df503f1546956
Dave Townsend <dtownsend@oxymoronical.com>
2020-02-02 21:27:27 +0000

    Remove unused code.

--- src/parser.ts
+++ src/parser.ts
@@ -73,20 +73,20 @@ export function parseCode(code: string, parserId: string, options: Linter.Parser
 
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   let parser = require(parserId) as ParserModule;
-  if ("parse" in parser) {
-    let program = parser.parse(code, parserOptions);
+  if ("parseForESLint" in parser) {
+    let { ast: program, scopeManager } = parser.parseForESLint(code, parserOptions);
     addParents(program);
     return {
       program,
-      scopeManager: buildScopeManager(program),
+      scopeManager: scopeManager || buildScopeManager(program),
     };
   }
 
-  let { ast: program, scopeManager } = parser.parseForESLint(code, parserOptions);
+  let program = parser.parse(code, parserOptions);
   addParents(program);
   return {
     program,
-    scopeManager: scopeManager || buildScopeManager(program),
+    scopeManager: buildScopeManager(program),
   };
 }
 
@@ -389,18 +389,6 @@ export function* exportEntries(module: SourceTextModuleRecord, program: ESTree.P
   }
 }
 
-function isFunctionVariableUsedInGlobalPath(scopeManager: ScopeManager, variable: Variable): boolean {
-  for (let reference of variable.references) {
-    if (isParented(reference.identifier) && reference.identifier.parent.type == "CallExpression") {
-      if (isInGlobalPath(scopeManager, reference.from)) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
-
 const BASE_SCOPES = ["class", "function", "global"];
 
 export function getBaseScope(scope: Scope): Scope {
@@ -466,70 +454,3 @@ export function getFunctionVariable(module: SourceTextModuleRecord, scopeManager
       return null;
   }
 }
-
-export function isInGlobalPath(scopeManager: ScopeManager, scope: Scope | null): boolean {
-  while (scope) {
-    if (scope.type == "class") {
-      return false;
-    }
-
-    if (scope.type == "function") {
-      if (scope.block.type == "FunctionDeclaration") {
-        let id = scope.block.id;
-        if (!id) {
-          // Part of an `export default function...`, can't be in use.
-          return false;
-        }
-
-        // Get the variable for the function and see if it is ever called.
-        let variable = scopeManager.getDeclaredVariables(scope.block)[0];
-        return isFunctionVariableUsedInGlobalPath(scopeManager, variable);
-      }
-
-      if (scope.block.type == "ArrowFunctionExpression" || scope.block.type == "FunctionExpression") {
-        if (!isParented(scope.block)) {
-          return false;
-        }
-
-        let variables = scopeManager.getDeclaredVariables(scope.block.parent);
-        if (variables.length > 0) {
-          return isFunctionVariableUsedInGlobalPath(scopeManager, variables[0]);
-        }
-
-        return false;
-      }
-
-      internalWarning(`Used in unknown function block ${scope.block.type}.`);
-      return false;
-    }
-
-    if (scope.type == "module") {
-      return true;
-    }
-
-    scope = scope.upper;
-  }
-
-  return false;
-}
-
-/**
- * Attempts to find a case where this import entry is used during a module's
- * initial execution.
- */
-// export function findImportUsage(scopeManager: ScopeManager, importEntry: ImportEntry): void {
-//   let variables = scopeManager.getDeclaredVariables(importEntry.node);
-//   for (let variable of variables) {
-//     for (let reference of variable.references) {
-//       if (isParented(reference.identifier) && reference.identifier.parent.type == "ExportSpecifier") {
-//         // Simply exporting the import does not count as usage since it will get
-//         // resolved directly to the imported module.
-//         continue;
-//       }
-
-//       if (isInGlobalPath(scopeManager, reference.from)) {
-//         importEntry.executionUse.push(reference.identifier);
-//       }
-//     }
-//   }
-// }
